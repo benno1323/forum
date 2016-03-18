b7d878f484b51b67a66a55a6ab54ba776cd33ba5
Benito Miranda <benomir82@gmail.com>
2016-03-17 21:52:48 -0600

    Validate user_id presence

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,5 @@ class Topic < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	belongs_to :category
 	belongs_to :user
-	validates :subject, :body, presence: true
+	validates :subject, :body, :user_id, presence: true
 end
