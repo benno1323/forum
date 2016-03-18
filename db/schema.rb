ad810a24ea027e38b9f5b78c6143898ca82b47d0
Benito Miranda <benomir82@gmail.com>
2016-03-17 21:49:06 -0600

    Add user id to Topics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317062137) do
+ActiveRecord::Schema.define(version: 20160317231720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,9 +37,11 @@ ActiveRecord::Schema.define(version: 20160317062137) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.integer  "user_id"
   end
 
   add_index "topics", ["category_id"], name: "index_topics_on_category_id", using: :btree
+  add_index "topics", ["user_id"], name: "index_topics_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -61,4 +63,5 @@ ActiveRecord::Schema.define(version: 20160317062137) do
 
   add_foreign_key "comments", "topics"
   add_foreign_key "topics", "categories"
+  add_foreign_key "topics", "users"
 end
