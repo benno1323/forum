debe179b1ba4276465ff3f564af7d599f92cef55
Benito Miranda <benomir82@gmail.com>
2016-03-17 21:49:59 -0600

    Add user association

--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :topic do
   	association :category
+  	association :user
     subject { Faker::Lorem.sentence }
 		body { Faker::Lorem.sentences(3).join(' ') }
   end
