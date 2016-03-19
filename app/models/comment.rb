938a9aa047e23216fe88503023e67efe1f9282ef
Benito Miranda <benomir82@gmail.com>
2016-03-19 00:22:06 -0600

    Add user_id presence validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :topic
+  belongs_to :user
 
-  validates :content, :topic_id, presence: true
+  validates :content, :topic_id, :user_id, presence: true
 end
