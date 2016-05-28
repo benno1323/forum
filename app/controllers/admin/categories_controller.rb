b5d6d7a9bf775205b454de7f56e2d95915c60c52
Benito Miranda <benomir82@gmail.com>
2016-05-28 16:16:04 -0500

    Add find_category method

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,47 @@
 class Admin::CategoriesController < Admin::BaseController
+	before_action :find_category, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@categories = Category.all
+	end
+
+	def new
+		@category = Category.new
+	end
+
+	def create
+		@category = Category.new(categories_params)
+
+		if @category.save
+			redirect_to admin_categories_path, notice: 'Category created successfully!'
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @category.update(categories_params)
+			redirect_to admin_categories_path, notice: 'Category updated successfully!'
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@category.destroy
+		redirect_to admin_categories_path
+	end
+
+	private
+
+	def categories_params
+		params.require(:category).permit(:name)
+	end
+
+	def find_category
+		@category = Category.find_by_id(params)
 	end
 end
