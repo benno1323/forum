e77f3bbb9f6f80bc014c8353e4cca556e6bed485
Benito Miranda <benomir82@gmail.com>
2016-05-27 23:46:44 -0500

    Add user authentication

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,2 +1,9 @@
 class Admin::BaseController < ApplicationController
+	layout 'admin'
+	before_action :authenticate_user!
+
+	private
+		def authenticate_admin
+			redirect_to root_path, notice: "Not authorized" unless current_user.admin?
+		end
 end
