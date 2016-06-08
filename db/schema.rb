45e03b35b13084c6f23b6af3371317704d5d1d9c
Benito Miranda <benomir82@gmail.com>
2016-06-08 00:24:59 -0500

    Add User Id to Categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525223732) do
+ActiveRecord::Schema.define(version: 20160606053642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20160525223732) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "categories", ["user_id"], name: "index_categories_on_user_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -65,6 +68,7 @@ ActiveRecord::Schema.define(version: 20160525223732) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "categories", "users"
   add_foreign_key "comments", "topics"
   add_foreign_key "comments", "users"
   add_foreign_key "topics", "categories"
