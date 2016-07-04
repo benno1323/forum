e4fbf7da868fed3b8349559f0131c8f02d474531
Benito Miranda <benomir82@gmail.com>
2016-07-04 17:58:29 -0500

    Add topic with comments factory

--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -1,8 +1,18 @@
 FactoryGirl.define do
-  factory :topic do
-  	association :category
-  	association :user
-    subject { Faker::Lorem.sentence }
+	factory :topic do
+		association :category
+		association :user
+		subject { Faker::Lorem.sentence }
 		body { Faker::Lorem.sentences(3).join(' ') }
+
+		factory :topic_with_comments do
+      transient do
+      	comments_count 5
+      end
+
+      after(:create) do |topic, evaluator|
+      	create_list(:comment, evaluator.comments_count, topic: topic)
+      end
+    end
   end
 end
