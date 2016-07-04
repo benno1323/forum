8c424c8969d7a648151464744be4ea4a94f5c738
Benito Miranda <benomir82@gmail.com>
2016-07-04 16:17:39 -0500

    Set sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,20 @@
 Comment.destroy_all
 Topic.destroy_all
 Category.destroy_all
+User.destroy_all
+
+admin = User.create(email: 'benimir@hotmail.com', password: '12345678', password_confirmation: '12345678', role: 2)
+user = User.create(email: 'mail@example.com', password: '12345678', password_confirmation: '12345678', role: 0)
+commenter = User.create(email: 'mail1@example.com', password: '12345678', password_confirmation: '12345678', role: 0)
 
 5.times do |i|
-	cat = Category.create(name: "Category #{i}")
+	cat = Category.create(name: "Category #{i}", user: admin)
 
 	2.times do |j|
-		topic = cat.topics.build(subject: "Subject #{j}", body: "This is topic #{j}")
+		topic = cat.topics.build(subject: "Subject #{j}", body: "This is topic #{j}", user: user)
 		topic.save
 		3.times do |k|
-			comment = topic.comments.build(content: "This is comment #{k}")
+			comment = topic.comments.build(content: "This is comment #{k}", user: commenter)
 			comment.save
 		end
 	end
