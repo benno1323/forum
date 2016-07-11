aa02d513bc7d55509e41784cae14a0b0ce0d12a6
Benito Miranda <benomir82@gmail.com>
2016-07-10 20:31:03 -0500

    Configure Bullet Gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,9 @@ Rails.application.configure do
 
   # Set Devise url default options
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.console = true
+  end
 end
