7d11ba93adb627d0e2409c579cc3bd3c9f45f999
Benito Miranda <benomir82@gmail.com>
2016-07-16 23:43:21 -0500

    Add comment resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '', to: 'dashboard#index', as: '/'
 
-    resources :categories, :topics
+    resources :categories, :topics, :comments
   end
 
   resources :topics do
